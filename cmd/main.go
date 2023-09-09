8f7000c212d17ee8b08fb536f18e509cdfd3d898
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2023-09-09 02:01:12 -0500

    Add logging for server running and gql playground

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"app/config"
 	"app/database"
 	"app/router"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -17,5 +18,7 @@ func main() {
 	r.Use(middleware.Logger)
 	database.Connect()
 	router.SetupRoutes(r)
+	fmt.Println("Server is running on port http://localhost:3000")
+	fmt.Println("GraphQL playground is running on http://localhost:3000/api/graphql/playground")
 	http.ListenAndServe(":3000", r)
 }
